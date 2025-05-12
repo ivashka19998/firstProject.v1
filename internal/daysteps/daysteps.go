549903240d3ec316bb5243e6296716bafdae685b
Ivan Nanov <ivashka19998@gmail.com>
2025-05-13 00:21:21 +0400

    0000

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -43,7 +43,7 @@ func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
 	steps, duration, err := parsePackage(data)
 	if err != nil {
-		return fmt.Sprintf("", err)
+		return ""
 	}
 
 	distance := float64(steps) * stepLength / mInKm
